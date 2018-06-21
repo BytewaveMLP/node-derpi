28af3028a1862499de4a075b92da7ffcea18dec7
Eliot Partridge <shortcircuit369@gmail.com>
2018-06-21 14:12:50 -0500

    Properly type hint collection functions

--- lib/util/Collection.ts
+++ lib/util/Collection.ts
@@ -69,11 +69,11 @@ export abstract class Collection<T1, T2> implements AsyncIterable<T2> {
 	/**
 	 * Finds the first item in the collection matching some condition
 	 *
-	 * @param {Function} fn The function to filter elements with
+	 * @param {(value: T2) => boolean} fn The function to filter elements with
 	 * @returns {(Promise<T2 | null>)} The found value (null if no items in the collection that match)
 	 * @memberof Collection
 	 */
-	public async find(fn: Function): Promise<T2 | null> {
+	public async find(fn: (value: T2) => boolean): Promise<T2 | null> {
 		for await (const value of this) {
 			if (fn(value)) return value;
 		}
@@ -85,11 +85,11 @@ export abstract class Collection<T1, T2> implements AsyncIterable<T2> {
 	 * Maps all elements of the collection over some function
 	 *
 	 * @template T The type of array to return
-	 * @param {Function} fn The function to map to every element in the collection
+	 * @param {(value: T2) => T} fn The function to map to every element in the collection
 	 * @returns {Promise<T[]>} A Promise wrapping the array returned
 	 * @memberof Collection
 	 */
-	public async map<T>(fn: Function): Promise<T[]> {
+	public async map<T>(fn: (value: T2) => T): Promise<T[]> {
 		let arr: T[] = [];
 
 		for await (const value of this) {
@@ -102,11 +102,11 @@ export abstract class Collection<T1, T2> implements AsyncIterable<T2> {
 	/**
 	 * Returns true if some of the elements in the collection match some conditional function
 	 *
-	 * @param {Function} fn The function used to check each element
+	 * @param {(value: T2) => boolean} fn The function used to check each element
 	 * @returns {Promise<boolean>} A Promise wrapping a boolean stating whether or not the condition matched at least one element
 	 * @memberof Collection
 	 */
-	public async some(fn: Function): Promise<boolean> {
+	public async some(fn: (value: T2) => boolean): Promise<boolean> {
 		for await (const value of this) {
 			if (fn(value)) return true;
 		}
@@ -117,11 +117,11 @@ export abstract class Collection<T1, T2> implements AsyncIterable<T2> {
 	/**
 	 * Returns true if every element in the collection matches some conditional
 	 *
-	 * @param {Function} fn The function used to check each element
+	 * @param {(value: T2) => boolean} fn The function used to check each element
 	 * @returns {Promise<boolean>} A Promise wrapping a boolean stating whether or not the condition matched every element
 	 * @memberof Collection
 	 */
-	public async every(fn: Function): Promise<boolean> {
+	public async every(fn: (value: T2) => boolean): Promise<boolean> {
 		if (this._ids.length === 0) return false;
 
 		for await (const value of this) {
@@ -137,12 +137,12 @@ export abstract class Collection<T1, T2> implements AsyncIterable<T2> {
 	 * Similar to Array#reduce
 	 *
 	 * @template T The type of the accumulator
-	 * @param {Function} fn The function used to reduce each element ()
+	 * @param {(acc: T, value: T2) => T} fn The function used to reduce each element ()
 	 * @param {T} initialAcc The initial value for the accumulator
 	 * @returns {Promise<T>} A Promise wrapping the final accumulator value
 	 * @memberof Collection
 	 */
-	public async reduce<T>(fn: Function, initialAcc: T): Promise<T> {
+	public async reduce<T>(fn: (acc: T, value: T2) => T, initialAcc: T): Promise<T> {
 		let acc = initialAcc;
 
 		for await (const value of this) {
@@ -156,11 +156,11 @@ export abstract class Collection<T1, T2> implements AsyncIterable<T2> {
 	 * Identical to map, but returns this instead of an array
 	 *
 	 * @see map
-	 * @param {Function} fn The function to map over each element
+	 * @param {(value: T2) => any} fn The function to map over each element
 	 * @returns {Promise<Collection<T1, T2>>} The current collection
 	 * @memberof Collection
 	 */
-	public async tap(fn: Function): Promise<Collection<T1, T2>> {
+	public async tap(fn: (value: T2) => any): Promise<Collection<T1, T2>> {
 		for await (const value of this) {
 			fn(value);
 		}
