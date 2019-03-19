fa935b689c6ad0402b172ce199de533155f9bed1
Eliot Partridge <shortcircuit369@gmail.com>
2019-03-19 18:50:45 -0500

    Export Fetch strongly-typed options interfaces

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,14 @@ import { Fetch } from './util/Fetch';
 Fetch.setup();
 
 export { Fetch };
+
+export { SearchOptions } from './util/Fetch';
+export { ResultSortFormat } from './util/Fetch';
+export { ResultSortOrder } from './util/Fetch';
+export { ReverseImageSearchOptions } from './util/Fetch';
+
+export { DefaultFilters } from './util/DefaultFilters';
+
 export { Award } from './api/Award';
 export { Comment } from './api/Comment';
 export { Image } from './api/Image';
@@ -13,4 +21,3 @@ export { SearchResults } from './api/SearchResults';
 export { Tag } from './api/Tag';
 export { TagDetails } from './api/TagDetails';
 export { User } from './api/User';
-export { DefaultFilters } from './util/DefaultFilters';
